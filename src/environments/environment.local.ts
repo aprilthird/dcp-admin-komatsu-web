fa3ef7b5fd88c3767584a03fcf02886211f2f3e4
carlos <carlos.alfonso2.7.9@gmail.com>
2022-01-03 16:15:03 -0500

    [fix token path]

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -6,5 +6,5 @@ export const environment = {
   azureAccountName: "appinformes",
   azureContaineName: "dcp",
   azureSas:
-    "?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-03-03T23:39:46Z&st=2022-01-03T15:39:46Z&spr=https&sig=BU4Y9BHhsNT4EbY%2FM2eJZ7X9EYdNE4NUC9nbcSwFuc8%3D",
+    "sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-03-03T23:39:46Z&st=2022-01-03T15:39:46Z&spr=https&sig=BU4Y9BHhsNT4EbY%2FM2eJZ7X9EYdNE4NUC9nbcSwFuc8%3D",
 };
